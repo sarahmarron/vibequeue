ac774fa8ef2feb35f0eea0f6107ab339547e8f2e
sarahmarron <smarron2@illinois.edu>
2025-11-03 21:38:21 -0600

    Renamed PromptBox fields to Song Title and Artist

--- FrontEnd-CS222/src/App.js
+++ FrontEnd-CS222/src/App.js
@@ -183,45 +183,6 @@ class App extends React.Component {
           </div>
         </div>
 
-        <form onSubmit={this.handleSubmit}>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                {" "}
-                Author{" "}
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Name of the Poet/Author"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-              value={this.state.user}
-              name="user"
-              onChange={this.handleInput}
-            />
-          </div>
-
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Your Quote</span>
-            </div>
-            <textarea
-              className="form-control "
-              aria-label="With textarea"
-              placeholder="Tell us what you think of ....."
-              value={this.state.quote}
-              name="quote"
-              onChange={this.handleInput}
-            ></textarea>
-          </div>
-
-          <button type="submit" className="btn btn-primary mb-5">
-            Submit
-          </button>
-        </form>
-
         {/* Database button */}
         <div className="my-4">
           <button
