416b5e6971104544e2be9861108c785f8716462c
Radhep5 <147786241+Radhep5@users.noreply.github.com>
2025-11-17 16:52:09 -0600

    Backend Play and Pause endpoints

--- BackEnd-CS222/spotifyapp/core/util.py
+++ BackEnd-CS222/spotifyapp/core/util.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import timedelta
 from .credentials import CLIENT_ID, CLIENT_SECRET
 from requests import post, put, get
+import requests
 
 SPOTIFY_BASE_URL = "https://api.spotify.com/v1"
 
