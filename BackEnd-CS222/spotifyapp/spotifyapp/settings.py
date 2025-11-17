1b6d2253ea87228923267e5498b49473336aecee
Justin Horton <gm.horton.justin@gmail.com>
2025-11-17 16:13:36 -0600

    Initial .env Setup to hide Project Secrets
    Everyone must create a .env file in Backend/spotifyapp per the env_example.txt and set the openai api key to my key in the .env(NOT THE example txt), later we will move all secret keys into this file but for now only the openai key, EVERYONE MUST CREATE A .env file

--- BackEnd-CS222/spotifyapp/spotifyapp/settings.py
+++ BackEnd-CS222/spotifyapp/spotifyapp/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # looks for .env
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
